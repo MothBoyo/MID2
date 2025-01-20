b1b539b1319ba9ee43e3086b848f04bb4c340317
Moth Boyo <iga>
2025-01-20 16:25:00 +0100

    second commit. mostly castle grounds stuff

--- levels/castle_grounds/texscroll.inc.c
+++ levels/castle_grounds/texscroll.inc.c
@@ -1,3 +1,10 @@
+void scroll_gfx_mat_castle_grounds_dl_decor_2_layer1() {
+	Gfx *mat = segmented_to_virtual(mat_castle_grounds_dl_decor_2_layer1);
+
+	shift_s(mat, 17, PACK_TILESIZE(0, 1));
+
+};
+
 void scroll_gfx_mat_castle_grounds_dl_water_layer1() {
 	Gfx *mat = segmented_to_virtual(mat_castle_grounds_dl_water_layer1);
 
@@ -15,6 +22,7 @@ void scroll_gfx_mat_castle_grounds_dl_decor_3_001_layer1() {
 };
 
 void scroll_castle_grounds() {
+	scroll_gfx_mat_castle_grounds_dl_decor_2_layer1();
 	scroll_gfx_mat_castle_grounds_dl_water_layer1();
 	scroll_gfx_mat_castle_grounds_dl_decor_3_001_layer1();
 };
--- levels/castle_grounds/texscroll.inc.h
+++ levels/castle_grounds/texscroll.inc.h
@@ -1,3 +1,4 @@
+extern void scroll_gfx_mat_castle_grounds_dl_decor_2_layer1();
 extern void scroll_gfx_mat_castle_grounds_dl_water_layer1();
 extern void scroll_gfx_mat_castle_grounds_dl_decor_3_001_layer1();
 extern void scroll_castle_grounds();
