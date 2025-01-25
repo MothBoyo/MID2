39da63b07a8ab9e39f6f1d87c614881c26b90602
MothBoyo <iga65c@gmail.com>
2025-01-25 01:35:53 +0100

    WORKING AND FINISHED LOGO!!!!!!!

--- levels/intro/INTRO_SCREEN/header.h
+++ levels/intro/INTRO_SCREEN/header.h
@@ -1,15 +1,3 @@
-#ifndef INTRO_HEADER_H
-#define INTRO_HEADER_H
-
-#include "types.h"
-
-// geo
-extern const GeoLayout intro_geo_splash_screen[];
-extern const GeoLayout intro_geo_mario_head_regular[];
-extern const GeoLayout intro_geo_mario_head_dizzy[];
-extern const GeoLayout intro_geo_debug_level_select[];
-
-// leveldata
 extern u8 INTRO_SCREEN_tile_0_0_rgba16[];
 extern u8 INTRO_SCREEN_tile_0_1_rgba16[];
 extern u8 INTRO_SCREEN_tile_0_2_rgba16[];
@@ -202,19 +190,3 @@ extern Gfx mat_revert_INTRO_SCREEN_tile_corner_png_f3d[];
 extern Gfx mat_INTRO_SCREEN_METAL[];
 extern Gfx mat_revert_INTRO_SCREEN_METAL[];
 extern Gfx INTRO_SCREEN_Image_mesh[];
-extern const Gfx intro_seg7_dl_copyright_trademark[];
-extern const f32 intro_seg7_table_scale_1[];
-extern const f32 intro_seg7_table_scale_2[];
-
-// script
-extern const LevelScript level_intro_splash_screen[];
-extern const LevelScript level_intro_mario_head_regular[];
-extern const LevelScript level_intro_mario_head_dizzy[];
-extern const LevelScript level_intro_entry_level_select[];
-extern const LevelScript script_intro_file_select[];
-extern const LevelScript script_intro_level_select[];
-extern const LevelScript script_intro_main_level_entry_stop_music[];
-extern const LevelScript script_intro_main_level_entry[];
-extern const LevelScript script_intro_splash_screen[];
-
-#endif
